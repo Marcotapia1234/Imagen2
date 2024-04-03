1e19d940d6791cd265f276a8de2ebffb6046c0b8
Marco <empleomarcotapia@gmail.com>
2024-04-03 07:19:54 -0700

    Initial Commit

---
+++ Imagen2/Imagen2App.swift
@@ -0,0 +1,17 @@
+//
+//  Imagen2App.swift
+//  Imagen2
+//
+//  Created by Marco on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Imagen2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
