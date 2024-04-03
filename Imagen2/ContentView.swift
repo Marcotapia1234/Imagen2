b47aa40a0e8eecfd4e0b330324b28fba1a8b40e6
Marco <empleomarcotapia@gmail.com>
2024-04-03 10:59:09 -0700

    Trabajar con imágenes 2

--- Imagen2/ContentView.swift
+++ Imagen2/ContentView.swift
@@ -8,10 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+    @State private var colorChange = false
+    @State private var sizeChange = false
+    
+       var body: some View {
+           VStack{
+           
+               Image(systemName: "sun.max.circle.fill")
+                   .font(.system(size: 150))
+                   .foregroundColor(colorChange ? .black : .orange)
+                   .scaleEffect(sizeChange ? 1.5 : 1)
+               Image(systemName: "heart.fill")
+               .font(.system(size: 200))
+                .foregroundColor(colorChange ? .yellow : .red)
+                .scaleEffect(sizeChange ? 1.5 : 1)
+                //.animation(.default)
+                .onTapGesture{
+                    self.colorChange.toggle()
+                    
+                }
+                .onLongPressGesture{
+                    self.sizeChange.toggle()
+                }
+           }
+       }
 }
 
 struct ContentView_Previews: PreviewProvider {
